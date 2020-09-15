9785eefce476609b0bcc0a2e02e6a7e74815a458
Sarthak Agrawal <sarthavader@gmail.com>
2020-09-15 10:33:48 -0500

    Initial Commit

---
+++ dodge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dodge
+//
+//  Created by 64000774 on 9/15/20.
+//  Copyright © 2020 64000774. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
